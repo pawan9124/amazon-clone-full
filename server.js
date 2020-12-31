c6a8ed2239ea9f012967baae2a97bc02f03c9660
Pawan Saket <pawan@vujadelabs.com>
2021-01-01 00:20:18 +0530

    Adding port setup on the react

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ import products from "./routes/products.js";
 
 /* App configuration */
 const app = express();
-const port = process.env.PORT || 4000;
+const port = 4000;
 
 /* middlewares */
 app.use(
